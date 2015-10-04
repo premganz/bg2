495480046fa08d04d50a40ce6122f26541f5396f
kvprem <kvprem@kv-OptiPlex-GX620>
2015-10-05 01:27:55 +0530

    fx

--- src/main/java/org/spo/ifs/config/SpringSecurityConfig.java
+++ src/main/java/org/spo/ifs/config/SpringSecurityConfig.java
@@ -31,7 +31,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().permitAll();
 
-        http.formLogin().loginProcessingUrl("/login").failureUrl(
+        http.formLogin().failureUrl(
                 "/login?error").defaultSuccessUrl("/");
         http.logout().logoutUrl("logout").logoutSuccessUrl("/login?loggedOut");
         http.httpBasic();
